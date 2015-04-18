a4723057818be02da908548739203bd9593d548f
BamBalooon <k.bb@wp.pl>
2015-04-18 10:24:17 +0200

    SC# ArcToState and ArcToTransition implementations of Arc defined

---
+++ petrilab-model/src/main/java/pl/edu/agh/eis/petrilab/model/ArcToState.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.eis.petrilab.model;
+
+/**
+ * Name: ArcToState
+ * Description: ArcToState
+ * Date: 2015-04-18
+ * Created by BamBalooon
+ */
+public class ArcToState extends Arc {
+    public ArcToState(State state, Transition transition) {
+        super(state, transition);
+    }
+
+    public ArcToState(State state, Transition transition, int weight) {
+        super(state, transition, weight);
+    }
+
+    public Transition getStartTransition() {
+        return transition;
+    }
+
+    public State getEndState() {
+        return state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArcToState)) return false;
+
+        return super.equals(o);
+    }
+}
---
+++ petrilab-model/src/main/java/pl/edu/agh/eis/petrilab/model/ArcToTransition.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.eis.petrilab.model;
+
+/**
+ * Name: ArcToTransition
+ * Description: ArcToTransition
+ * Date: 2015-04-18
+ * Created by BamBalooon
+ */
+public class ArcToTransition extends Arc {
+    public ArcToTransition(State state, Transition transition) {
+        super(state, transition);
+    }
+
+    public ArcToTransition(State state, Transition transition, int weight) {
+        super(state, transition, weight);
+    }
+
+    public State getStartState() {
+        return state;
+    }
+
+    public Transition getEndTransition() {
+        return transition;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArcToTransition)) return false;
+
+        return super.equals(o);
+    }
+}
