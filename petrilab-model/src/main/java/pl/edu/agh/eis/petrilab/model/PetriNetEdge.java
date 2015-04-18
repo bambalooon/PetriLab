2e955768e2ee3b75571fffc3ffa0f7cc94c0c977
BamBalooon <k.bb@wp.pl>
2015-04-18 12:56:20 +0200

    SC# added PetriNetEdge and PetriNetVertex interfaces

---
+++ petrilab-model/src/main/java/pl/edu/agh/eis/petrilab/model/PetriNetEdge.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.eis.petrilab.model;
+
+/**
+ * Name: PetriNetEdge
+ * Description: PetriNetEdge
+ * Date: 2015-04-18
+ * Created by BamBalooon
+ */
+public interface PetriNetEdge {
+}
---
+++ petrilab-model/src/main/java/pl/edu/agh/eis/petrilab/model/PetriNetVertex.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.eis.petrilab.model;
+
+/**
+ * Name: PetriNetVertex
+ * Description: PetriNetVertex
+ * Date: 2015-04-18
+ * Created by BamBalooon
+ */
+public interface PetriNetVertex {
+}
