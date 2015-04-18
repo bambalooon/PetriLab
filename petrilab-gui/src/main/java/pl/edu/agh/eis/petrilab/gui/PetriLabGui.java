7f4d309fac0a983be1e5c8e2bde1f16a20f9d1b7
BamBalooon <k.bb@wp.pl>
2015-04-18 15:54:55 +0200

    RF# added menu toolbar with options to add Place and Transition

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/PetriLabGui.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/PetriLabGui.java
@@ -8,6 +8,8 @@ import pl.edu.agh.eis.petrilab.model.PetriNetVertex;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Name: PetriLabGui
@@ -23,14 +25,42 @@ public class PetriLabGui extends JFrame {
     public PetriLabGui() {
         super(PetriLabApplication.TITLE);
         this.petriNet = new PetriNet();
-        setUpFrame(VisualizationViewerGenerator.PETRI_NET.generateVisualizationViewer(petriNet.getGraph()));
+        setUpFrame(VisualizationViewerGenerator.PETRI_NET.generateVisualizationViewer(petriNet.getGraph()), new Menu());
     }
 
-    private void setUpFrame(VisualizationViewer<PetriNetVertex, Arc> visualizationViewer) {
+    private void setUpFrame(VisualizationViewer<PetriNetVertex, Arc> graphViewer, JComponent menu) {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
         setLayout(new BorderLayout());
-        getContentPane().add(visualizationViewer);
+        getContentPane().add(graphViewer, BorderLayout.CENTER);
+        getContentPane().add(menu, BorderLayout.EAST);
         pack();
+    }
+
+    private class Menu extends JToolBar implements ActionListener {
+        private static final String PETRI_LAB_TOOLBAR = "PetriLab Toolbar";
+        private static final String ADD_PLACE_CMD = "ADD_PLACE_CMD";
+        private static final String ADD_TRANSITION_CMD = "ADD_TRANSITION_CMD";
+
+        public Menu() {
+            super(PETRI_LAB_TOOLBAR, JToolBar.VERTICAL);
+            JButton addPlaceBtn = generateButton(ADD_PLACE_CMD, "P");
+            JButton addTransitionBtn = generateButton(ADD_TRANSITION_CMD, "T");
+            add(addPlaceBtn);
+            add(addTransitionBtn);
+        }
+
+        private JButton generateButton(String actionCmd, String text) {
+            JButton button = new JButton();
+            button.addActionListener(this);
+            button.setActionCommand(actionCmd);
+            button.setText(text);
+            return button;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
