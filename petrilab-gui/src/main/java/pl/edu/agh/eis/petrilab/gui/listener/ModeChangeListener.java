c7a692045903b3d65033d8b399c89d83d44cd2ae
BamBalooon <k.bb@wp.pl>
2015-04-19 17:43:01 +0200

    RF# arc edition canceled on mode change

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/listener/ModeChangeListener.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/listener/ModeChangeListener.java
@@ -24,6 +24,7 @@ public class ModeChangeListener implements ItemListener {
         if (e.getStateChange() == ItemEvent.DESELECTED && e.getItem() == ModalGraphMouse.Mode.PICKING) {
             menu.cancelPlace();
             menu.cancelTransition();
+            menu.cancelArc();
         }
     }
 }
