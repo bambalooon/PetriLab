f6521ee2fd2fc45b6fc1ca14cb0ede90692a50f2
BamBalooon <k.bb@wp.pl>
2015-04-19 14:07:59 +0200

    RF# PetriNetEditionMenu now takes in constructor VisualizationViewer to update graph

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/PetriNetEditionMenu.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/PetriNetEditionMenu.java
@@ -1,6 +1,9 @@
 package pl.edu.agh.eis.petrilab.gui.menu;
 
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import pl.edu.agh.eis.petrilab.model.Arc;
 import pl.edu.agh.eis.petrilab.model.PTPlace;
+import pl.edu.agh.eis.petrilab.model.PetriNetVertex;
 
 import javax.swing.*;
 
@@ -13,9 +16,10 @@ import javax.swing.*;
 public class PetriNetEditionMenu extends JPanel {
     public static final int TEXT_FIELD_WIDTH = 50;
     public static final int TEXT_FIELD_HEIGHT = 20;
-    private PTPlaceEditionPanel placeEditionPanel = new PTPlaceEditionPanel();
+    private final PTPlaceEditionPanel placeEditionPanel;
 
-    public PetriNetEditionMenu() {
+    public PetriNetEditionMenu(VisualizationViewer<PetriNetVertex, Arc> graphViewer) {
+        placeEditionPanel = new PTPlaceEditionPanel(graphViewer);
         add(placeEditionPanel);
     }
 
