1419751820c6b08417b470571fab36c5cc535f1c
BamBalooon <k.bb@wp.pl>
2015-04-19 15:28:18 +0200

    RF# added transitionEditionPanel to menu, removed accept methods - unused

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/PetriNetEditionMenu.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/PetriNetEditionMenu.java
@@ -2,6 +2,7 @@ package pl.edu.agh.eis.petrilab.gui.menu;
 
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import pl.edu.agh.eis.petrilab.model.Arc;
+import pl.edu.agh.eis.petrilab.model.GeneralTransition;
 import pl.edu.agh.eis.petrilab.model.PTPlace;
 import pl.edu.agh.eis.petrilab.model.PetriNetVertex;
 
@@ -17,21 +18,28 @@ public class PetriNetEditionMenu extends JPanel {
     public static final int TEXT_FIELD_WIDTH = 50;
     public static final int TEXT_FIELD_HEIGHT = 20;
     private final PTPlaceEditionPanel placeEditionPanel;
+    private final GeneralTransitionEditionPanel transitionEditionPanel;
 
     public PetriNetEditionMenu(VisualizationViewer<PetriNetVertex, Arc> graphViewer) {
         placeEditionPanel = new PTPlaceEditionPanel(graphViewer);
+        transitionEditionPanel = new GeneralTransitionEditionPanel(graphViewer);
         add(placeEditionPanel);
+        add(transitionEditionPanel);
     }
 
     public void editPlace(PTPlace place) {
         placeEditionPanel.edit(place);
     }
 
-    public void acceptPlace() {
-        placeEditionPanel.accept();
-    }
-
     public void cancelPlace() {
         placeEditionPanel.cancel();
+    }
+
+    public void editTransition(GeneralTransition transition) {
+        transitionEditionPanel.edit(transition);
+    }
+
+    public void cancelTransition() {
+        transitionEditionPanel.cancel();
     }
 }
