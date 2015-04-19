abc5087884f7c5256e3d42a9f5b68670b599d888
BamBalooon <k.bb@wp.pl>
2015-04-19 14:07:01 +0200

    SC# added accept button and event to PTPlaceEditionPanel, when place edition is accepted - graph is repainted, edition menu is no longer canceled when changes are accepted

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/PTPlaceEditionPanel.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/PTPlaceEditionPanel.java
@@ -1,10 +1,15 @@
 package pl.edu.agh.eis.petrilab.gui.menu;
 
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import pl.edu.agh.eis.petrilab.model.Arc;
 import pl.edu.agh.eis.petrilab.model.GeneralPlace;
 import pl.edu.agh.eis.petrilab.model.PTPlace;
+import pl.edu.agh.eis.petrilab.model.PetriNetVertex;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Name: PTPlaceEditionPanel
@@ -12,14 +17,19 @@ import java.awt.*;
  * Date: 2015-04-19
  * Created by BamBalooon
  */
-public class PTPlaceEditionPanel extends JPanel {
+public class PTPlaceEditionPanel extends JPanel implements ActionListener {
+    private static final String ACCEPT_BUTTON_LABEL = "ok";
+    private static final String ACCEPT_BUTTON_CMD = "ACCEPT_BUTTON_CMD";
     private final JTextField nameField;
     private final JSpinner markingSpinner;
     private final JSpinner capacitySpinner;
+    private final JButton acceptButton;
+    private final VisualizationViewer<PetriNetVertex, Arc> graphViewer;
     private PTPlace place;
 
-    public PTPlaceEditionPanel() {
+    public PTPlaceEditionPanel(VisualizationViewer<PetriNetVertex, Arc> graphViewer) {
         setVisible(false);
+        this.graphViewer = graphViewer;
         nameField = new JTextField();
         nameField.setPreferredSize(
                 new Dimension(PetriNetEditionMenu.TEXT_FIELD_WIDTH, PetriNetEditionMenu.TEXT_FIELD_HEIGHT));
@@ -35,9 +45,14 @@ public class PTPlaceEditionPanel extends JPanel {
                 PTPlace.CAPACITY_MAX,
                 1);
         capacitySpinner = new JSpinner(capacityModel);
+        acceptButton = new JButton();
+        acceptButton.setActionCommand(ACCEPT_BUTTON_CMD);
+        acceptButton.addActionListener(this);
+        acceptButton.setText(ACCEPT_BUTTON_LABEL);
         add(nameField);
         add(markingSpinner);
         add(capacitySpinner);
+        add(acceptButton);
     }
 
     public void edit(PTPlace place) {
@@ -56,7 +71,7 @@ public class PTPlaceEditionPanel extends JPanel {
                 .withMarking((Integer) markingSpinner.getValue())
                 .withCapacity((Integer) capacitySpinner.getValue())
                 .modify();
-        cancel();
+        graphViewer.repaint();
     }
 
     public void cancel() {
@@ -65,4 +80,12 @@ public class PTPlaceEditionPanel extends JPanel {
         revalidate();
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case ACCEPT_BUTTON_CMD:
+                accept();
+                break;
+        }
+    }
 }
