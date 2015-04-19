56a28a9691f7cb61fa96624e1112bd4cc177eb3c
BamBalooon <k.bb@wp.pl>
2015-04-19 15:03:13 +0200

    RF# added ModeChangeListener that closes edition panels of place and transition

---
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/listener/ModeChangeListener.java
@@ -0,0 +1,29 @@
+package pl.edu.agh.eis.petrilab.gui.listener;
+
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import pl.edu.agh.eis.petrilab.gui.menu.PetriNetEditionMenu;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+/**
+ * Name: ModeChangeListener
+ * Description: ModeChangeListener
+ * Date: 2015-04-19
+ * Created by BamBalooon
+ */
+public class ModeChangeListener implements ItemListener {
+    private final PetriNetEditionMenu menu;
+
+    public ModeChangeListener(PetriNetEditionMenu menu) {
+        this.menu = menu;
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if (e.getStateChange() == ItemEvent.DESELECTED && e.getItem() == ModalGraphMouse.Mode.PICKING) {
+            menu.cancelPlace();
+            menu.cancelTransition();
+        }
+    }
+}
