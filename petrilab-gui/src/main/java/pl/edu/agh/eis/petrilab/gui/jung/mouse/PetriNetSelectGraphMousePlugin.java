5cd5dead063fa48efd65030cc5a5c513a9f172bb
BamBalooon <k.bb@wp.pl>
2015-05-16 17:25:59 +0200

    RF# added selection function to plugin

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/jung/mouse/PetriNetSelectGraphMousePlugin.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/jung/mouse/PetriNetSelectGraphMousePlugin.java
@@ -3,9 +3,11 @@ package pl.edu.agh.eis.petrilab.gui.jung.mouse;
 import edu.uci.ics.jung.algorithms.layout.GraphElementAccessor;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.AbstractGraphMousePlugin;
+import edu.uci.ics.jung.visualization.picking.PickedState;
 import pl.edu.agh.eis.petrilab.model.Arc;
+import pl.edu.agh.eis.petrilab.model.GeneralTransition;
+import pl.edu.agh.eis.petrilab.model.PTPlace;
 import pl.edu.agh.eis.petrilab.model.PetriNetVertex;
-import pl.edu.agh.eis.petrilab.model.Transition;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -18,10 +20,13 @@ import java.awt.geom.Point2D;
  * Created by BamBalooon
  */
 public class PetriNetSelectGraphMousePlugin extends AbstractGraphMousePlugin implements MouseListener {
+    private PTPlace selectedFromPlace;
+    private PTPlace selectedToPlace;
+    private GeneralTransition selectedTransition;
+
     public PetriNetSelectGraphMousePlugin() {
         super(MouseEvent.BUTTON1_MASK);
     }
-
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -33,11 +38,48 @@ public class PetriNetSelectGraphMousePlugin extends AbstractGraphMousePlugin imp
             if (pickSupport != null) {
                 PetriNetVertex vertex = pickSupport
                         .getVertex(eventVv.getGraphLayout(), eventPoint.getX(), eventPoint.getY());
-                if (vertex instanceof Transition) {
-                    System.out.println("Transition clicked!");
+
+                if (vertex instanceof PTPlace) {
+                    if (selectedFromPlace == null) {
+                        selectedFromPlace = (PTPlace) vertex;
+                    } else if (selectedToPlace == null) {
+                        selectedToPlace = (PTPlace) vertex;
+                    } else {
+                        //TODO: add error message popup
+                        clearSelection(eventVv);
+                        return;
+                    }
+                } else if (vertex instanceof  GeneralTransition) {
+                    if (selectedTransition == null) {
+                        selectedTransition = (GeneralTransition) vertex;
+                    } else {
+                        //TODO: add error message popup
+                        clearSelection(eventVv);
+                        return;
+                    }
                 }
+                pickVertices(eventVv.getPickedVertexState());
             }
         }
+    }
+
+    private void pickVertices(PickedState<PetriNetVertex> pickedVertexState) {
+        if (selectedFromPlace != null) {
+            pickedVertexState.pick(selectedFromPlace, true);
+        }
+        if (selectedToPlace != null) {
+            pickedVertexState.pick(selectedToPlace, true);
+        }
+        if (selectedTransition != null) {
+            pickedVertexState.pick(selectedTransition, true);
+        }
+    }
+
+    private void clearSelection(VisualizationViewer<PetriNetVertex, Arc> eventVv) {
+        eventVv.getPickedVertexState().clear();
+        selectedFromPlace = null;
+        selectedToPlace = null;
+        selectedTransition = null;
     }
 
     @Override
