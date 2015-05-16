078d233d0ca1646a85abfa1a34248b9afe5e4fe3
BamBalooon <k.bb@wp.pl>
2015-05-16 13:44:40 +0200

    RF# refactored PetriNetGui, it uses now MenuPanel and creates tab for simulation

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/PetriLabGui.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/PetriLabGui.java
@@ -1,13 +1,19 @@
 package pl.edu.agh.eis.petrilab.gui;
 
+import com.google.common.collect.ImmutableList;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
 import edu.uci.ics.jung.visualization.picking.PickedState;
 import pl.edu.agh.eis.petrilab.gui.jung.PetriNetManager;
 import pl.edu.agh.eis.petrilab.gui.jung.PetriNetModalGraphMouse;
 import pl.edu.agh.eis.petrilab.gui.jung.VisualizationViewerGenerator;
 import pl.edu.agh.eis.petrilab.gui.listener.ModeChangeListener;
 import pl.edu.agh.eis.petrilab.gui.listener.PickListener;
-import pl.edu.agh.eis.petrilab.gui.menu.edition.PetriNetEditionMenu;
+import pl.edu.agh.eis.petrilab.gui.menu.MenuPanel;
+import pl.edu.agh.eis.petrilab.gui.menu.TabComponent;
+import pl.edu.agh.eis.petrilab.gui.menu.action.*;
+import pl.edu.agh.eis.petrilab.gui.menu.action.Action;
+import pl.edu.agh.eis.petrilab.gui.menu.edition.PetriNetEditionPanel;
 import pl.edu.agh.eis.petrilab.model.Arc;
 import pl.edu.agh.eis.petrilab.model.PetriNetVertex;
 
@@ -37,16 +43,10 @@ public class PetriLabGui extends JFrame {
 
         graphMouse.addItemListener(new ModeChangeListener(vertexPickedState, arcPickedState));
 
-        PetriNetEditionMenu menu = new PetriNetEditionMenu(graphViewer);
-
-        PickListener pickListener = new PickListener(vertexPickedState, arcPickedState, menu);
-        vertexPickedState.addItemListener(pickListener);
-        arcPickedState.addItemListener(pickListener);
-
-        setUpFrame(graphViewer, menu);
+        setUpFrame(graphViewer, setUpMenu(graphMouse, graphViewer));
     }
 
-    private void setUpFrame(VisualizationViewer<PetriNetVertex, Arc> graphViewer, JComponent menu) {
+    private void setUpFrame(VisualizationViewer<PetriNetVertex, Arc> graphViewer, Component menu) {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
         setLayout(new BorderLayout());
@@ -56,5 +56,25 @@ public class PetriLabGui extends JFrame {
         splitPane.setContinuousLayout(true);
         getContentPane().add(splitPane, BorderLayout.CENTER);
         pack();
+    }
+
+    private Component setUpMenu(PetriNetModalGraphMouse graphMouse,
+                                VisualizationViewer<PetriNetVertex, Arc> graphViewer) {
+
+        PetriNetEditionPanel petriNetEditionPanel = new PetriNetEditionPanel(graphMouse, graphViewer);
+        MenuPanel menuPanel = new MenuPanel(ImmutableList.<TabComponent>builder()
+                .add(new TabComponent<>("Edit", petriNetEditionPanel,
+                        new ModeChangeAction(graphMouse, ModalGraphMouse.Mode.PICKING), Action.NO_ACTION))
+                .add(new TabComponent<>("Simulation", new JPanel(),
+                        new ModeChangeAction(graphMouse, ModalGraphMouse.Mode.ANNOTATING), Action.NO_ACTION))
+                .build());
+
+        PickedState<PetriNetVertex> vertexPickedState = graphViewer.getPickedVertexState();
+        PickedState<Arc> arcPickedState = graphViewer.getPickedEdgeState();
+
+        PickListener pickListener = new PickListener(vertexPickedState, arcPickedState, petriNetEditionPanel);
+        vertexPickedState.addItemListener(pickListener);
+        arcPickedState.addItemListener(pickListener);
+        return menuPanel;
     }
 }
