b789512503cabe4ce9210c53c9d08d29b65abe65
BamBalooon <k.bb@wp.pl>
2015-05-19 23:53:26 +0200

    SC# added empty SimulationPanel

---
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/simulation/SimulationPanel.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.eis.petrilab.gui.menu.simulation;
+
+import javax.swing.JPanel;
+
+/**
+ * Name: SimulationPanel
+ * Description: SimulationPanel
+ * Date: 2015-05-19
+ * Created by BamBalooon
+ */
+public class SimulationPanel extends JPanel {
+}
