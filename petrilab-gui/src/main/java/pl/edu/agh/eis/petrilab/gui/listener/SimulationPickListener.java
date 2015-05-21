d6b5be9aa2b7e90981c97100ec4cbf31203f59b9
BamBalooon <k.bb@wp.pl>
2015-05-21 19:40:12 +0200

    SC# SimulationPickListener checks if transition is picked and then fires it and unpicks, graph is repainted

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/listener/SimulationPickListener.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/listener/SimulationPickListener.java
@@ -1,7 +1,12 @@
 package pl.edu.agh.eis.petrilab.gui.listener;
 
+import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.picking.PickedState;
+import pl.edu.agh.eis.petrilab.gui.PetriLabApplication;
+import pl.edu.agh.eis.petrilab.model2.Arc;
 import pl.edu.agh.eis.petrilab.model2.PetriNetVertex;
+import pl.edu.agh.eis.petrilab.model2.Transition;
+import pl.edu.agh.eis.petrilab.model2.jung.PetriNetGraph;
 
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
@@ -13,14 +18,25 @@ import java.awt.event.ItemListener;
  * Created by BamBalooon
  */
 public class SimulationPickListener implements ItemListener {
+    private final VisualizationViewer<PetriNetVertex, Arc> graphViewer;
     private final PickedState<PetriNetVertex> vertexPickedState;
 
-    public SimulationPickListener(PickedState<PetriNetVertex> vertexPickedState) {
+    public SimulationPickListener(VisualizationViewer<PetriNetVertex, Arc> graphViewer,
+                                  PickedState<PetriNetVertex> vertexPickedState) {
+        this.graphViewer = graphViewer;
         this.vertexPickedState = vertexPickedState;
     }
 
     @Override
     public void itemStateChanged(ItemEvent e) {
-        System.out.println("pick: " + e);
+        PetriNetGraph simulationGraph = PetriLabApplication.getInstance().getSimulationGraph();
+        if (e.getItem() instanceof Transition) {
+            Transition transition = (Transition) e.getItem();
+            if (vertexPickedState.isPicked(transition)) {
+                simulationGraph.fireTransition(transition);
+                vertexPickedState.pick(transition, false);
+                graphViewer.repaint();
+            }
+        }
     }
 }
