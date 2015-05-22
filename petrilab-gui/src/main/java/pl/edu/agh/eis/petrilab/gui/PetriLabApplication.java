fdb08d5122f20f0ce46610bd536335a0a2a5063d
BamBalooon <k.bb@wp.pl>
2015-05-22 21:58:12 +0200

    RF# graphViewer and graphMouse initialization moved to application class

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/PetriLabApplication.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/PetriLabApplication.java
@@ -1,6 +1,16 @@
 package pl.edu.agh.eis.petrilab.gui;
 
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.EditingModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import pl.edu.agh.eis.petrilab.gui.jung.VisualizationViewerGenerator;
+import pl.edu.agh.eis.petrilab.gui.jung.factory.ArcFactory;
+import pl.edu.agh.eis.petrilab.gui.jung.factory.VertexFactory;
+import pl.edu.agh.eis.petrilab.gui.listener.ModeChangeListener;
 import pl.edu.agh.eis.petrilab.gui.util.NameGenerator;
+import pl.edu.agh.eis.petrilab.model2.Arc;
+import pl.edu.agh.eis.petrilab.model2.PetriNetVertex;
 import pl.edu.agh.eis.petrilab.model2.jung.PetriNetGraph;
 import pl.edu.agh.eis.petrilab.model2.matrix.PetriNetMatrix;
 
@@ -24,9 +34,11 @@ public class PetriLabApplication {
     private PetriLabGui petriLabGui;
     private PetriNetGraph petriNetGraph = new PetriNetGraph();
     private PetriNetGraph simulationGraph;
+    private VisualizationViewer<PetriNetVertex, Arc> graphViewer;
+    private EditingModalGraphMouse<PetriNetVertex, Arc> graphMouse;
     private Configuration configuration = new Configuration();
 
-    public void startGui() {
+    private void startGui() {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -34,6 +46,21 @@ public class PetriLabApplication {
                 petriLabGui.setVisible(true);
             }
         });
+    }
+
+    private void initGraphViewer() {
+        graphViewer = VisualizationViewerGenerator.PETRI_NET.generateVisualizationViewer(petriNetGraph);
+
+        graphMouse = new EditingModalGraphMouse<>(graphViewer.getRenderContext(),
+                new VertexFactory(), new ArcFactory());
+
+        graphMouse.setMode(ModalGraphMouse.Mode.PICKING);
+
+        graphViewer.setGraphMouse(graphMouse);
+        graphViewer.addKeyListener(graphMouse.getModeKeyListener());
+
+        graphMouse.addItemListener(
+                new ModeChangeListener(graphViewer.getPickedVertexState(), graphViewer.getPickedEdgeState()));
     }
 
     public PetriNetGraph getPetriNetGraph() {
@@ -44,20 +71,30 @@ public class PetriLabApplication {
         return simulationGraph;
     }
 
-    public void loadPetriNetGraph() {
-        this.petriLabGui.loadPetriNetGraph();
+    public VisualizationViewer<PetriNetVertex, Arc> getGraphViewer() {
+        return graphViewer;
     }
 
-    public void loadPetriNetGraph(PetriNetGraph petriNetGraph) {
-        this.petriNetGraph = petriNetGraph;
-        this.petriLabGui.loadPetriNetGraph();
+    public EditingModalGraphMouse<PetriNetVertex, Arc> getGraphMouse() {
+        return graphMouse;
+    }
+
+    public void loadPetriNetGraph() {
+        graphViewer.setGraphLayout(new CircleLayout<>(petriNetGraph));
+        graphMouse.setMode(ModalGraphMouse.Mode.PICKING);
         NameGenerator.PLACE.setNameCounter(petriNetGraph.getPlaces().size() + 1);
         NameGenerator.TRANSITION.setNameCounter(petriNetGraph.getTransitions().size() + 1);
     }
 
+    public void loadPetriNetGraph(PetriNetGraph graph) {
+        petriNetGraph = graph;
+        loadPetriNetGraph();
+    }
+
     public void loadSimulationGraph() {
-        this.simulationGraph = PetriNetGraph.fromMatrix(PetriNetMatrix.generateMatrix(petriNetGraph));
-        this.petriLabGui.loadSimulationGraph();
+        simulationGraph = PetriNetGraph.fromMatrix(PetriNetMatrix.generateMatrix(petriNetGraph));
+        graphViewer.setGraphLayout(new CircleLayout<>(simulationGraph));
+        graphMouse.setMode(ModalGraphMouse.Mode.PICKING);
     }
 
     public Configuration getConfiguration() {
@@ -65,6 +102,7 @@ public class PetriLabApplication {
     }
 
     public static void main(String... args) {
+        PetriLabApplication.getInstance().initGraphViewer();
         PetriLabApplication.getInstance().startGui();
     }
 }
