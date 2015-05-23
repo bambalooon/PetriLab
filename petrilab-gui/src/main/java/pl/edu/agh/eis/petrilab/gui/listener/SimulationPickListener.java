39138011e902efd0b232fdd2eba04fadd9e1f4cd
BamBalooon <k.bb@wp.pl>
2015-05-24 00:42:38 +0200

    RF# SimulationPickListener sets marking on change

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/listener/SimulationPickListener.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/listener/SimulationPickListener.java
@@ -3,6 +3,7 @@ package pl.edu.agh.eis.petrilab.gui.listener;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.picking.PickedState;
 import pl.edu.agh.eis.petrilab.gui.PetriLabApplication;
+import pl.edu.agh.eis.petrilab.gui.menu.simulation.SimulationPanel;
 import pl.edu.agh.eis.petrilab.model2.Arc;
 import pl.edu.agh.eis.petrilab.model2.PetriNetVertex;
 import pl.edu.agh.eis.petrilab.model2.Transition;
@@ -10,6 +11,7 @@ import pl.edu.agh.eis.petrilab.model2.jung.PetriNetGraph;
 
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Arrays;
 
 /**
  * Name: SimulationPickListener
@@ -20,11 +22,14 @@ import java.awt.event.ItemListener;
 public class SimulationPickListener implements ItemListener {
     private final VisualizationViewer<PetriNetVertex, Arc> graphViewer;
     private final PickedState<PetriNetVertex> vertexPickedState;
+    private final SimulationPanel simulationPanel;
 
     public SimulationPickListener(VisualizationViewer<PetriNetVertex, Arc> graphViewer,
-                                  PickedState<PetriNetVertex> vertexPickedState) {
+                                  PickedState<PetriNetVertex> vertexPickedState,
+                                  SimulationPanel simulationPanel) {
         this.graphViewer = graphViewer;
         this.vertexPickedState = vertexPickedState;
+        this.simulationPanel = simulationPanel;
     }
 
     @Override
@@ -34,6 +39,7 @@ public class SimulationPickListener implements ItemListener {
             Transition transition = (Transition) e.getItem();
             if (vertexPickedState.isPicked(transition)) {
                 simulationGraph.fireTransition(transition);
+                simulationPanel.getMarkingLabel().setText(Arrays.toString(simulationGraph.getMarking()));
                 vertexPickedState.pick(transition, false);
                 graphViewer.repaint();
             }
