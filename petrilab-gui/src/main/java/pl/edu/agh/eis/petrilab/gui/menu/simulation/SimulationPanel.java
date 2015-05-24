fd2cccec682992a3ebf3dc48929aac9743d18089
BamBalooon <k.bb@wp.pl>
2015-05-24 21:49:52 +0200

    SC# added SimulationPlayer to SimulationPanel

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/simulation/SimulationPanel.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/simulation/SimulationPanel.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.eis.petrilab.gui.menu.simulation;
 
 import pl.edu.agh.eis.petrilab.gui.PetriLabApplication;
+import pl.edu.agh.eis.petrilab.gui.SimulationPlayer;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -29,6 +30,7 @@ public class SimulationPanel extends JPanel implements ActionListener, Observer 
     private static final String PAUSE_BUTTON_ACTION = "PAUSE_BUTTON_ACTION";
     private static final String PAUSE_BUTTON_RES = "/org/freedesktop/tango/32x32/actions/media-playback-pause.png";
     private final JLabel markingLabel = new JLabel();
+    private final SimulationPlayer simulationPlayer;
 
     public SimulationPanel() {
         PetriLabApplication.getInstance().getModeManager().addObserver(this);
@@ -44,6 +46,7 @@ public class SimulationPanel extends JPanel implements ActionListener, Observer 
 
         JCheckBox autoSimulationCheckBox = new JCheckBox("Auto");
         add(autoSimulationCheckBox, gbc);
+        this.simulationPlayer = new SimulationPlayer(autoSimulationCheckBox);
 
         gbc.gridwidth = 1;
         gbc.anchor = GridBagConstraints.NORTHWEST;
@@ -75,7 +78,17 @@ public class SimulationPanel extends JPanel implements ActionListener, Observer 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        switch (e.getActionCommand()) {
+            case PLAY_BUTTON_ACTION:
+                simulationPlayer.play();
+                break;
+            case PAUSE_BUTTON_ACTION:
+                simulationPlayer.pause();
+                break;
+            case STOP_BUTTON_ACTION:
+                simulationPlayer.stop();
+                break;
+        }
     }
 
     private JButton createButton(String action, String resource) {
