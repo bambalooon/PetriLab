fe2844b4f34e11efe1457ca8702fc06e9bb6590f
BamBalooon <k.bb@wp.pl>
2015-05-24 19:10:23 +0200

    RF# StartSimulationModeAction and StopSimulationModeAction now just change application mode

--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/action/StartSimulationModeAction.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/action/StartSimulationModeAction.java
@@ -1,15 +1,6 @@
 package pl.edu.agh.eis.petrilab.gui.menu.action;
 
-import edu.uci.ics.jung.visualization.VisualizationServer;
-import edu.uci.ics.jung.visualization.VisualizationViewer;
 import pl.edu.agh.eis.petrilab.gui.PetriLabApplication;
-import pl.edu.agh.eis.petrilab.gui.jung.paintable.ActiveTransitionsBoundingRectanglePaintable;
-import pl.edu.agh.eis.petrilab.gui.menu.simulation.SimulationPanel;
-import pl.edu.agh.eis.petrilab.model2.Arc;
-import pl.edu.agh.eis.petrilab.model2.PetriNetVertex;
-
-import static pl.edu.agh.eis.petrilab.gui.Configuration.SIMULATION_ACTIVE_TRANSITIONS_PRE_RENDER_PAINTABLE;
-import static pl.edu.agh.eis.petrilab.gui.Configuration.SIMULATION_MODE_ACTIVE;
 
 /**
  * Name: StartSimulationMode
@@ -18,25 +9,8 @@ import static pl.edu.agh.eis.petrilab.gui.Configuration.SIMULATION_MODE_ACTIVE;
  * Created by BamBalooon
  */
 public class StartSimulationModeAction implements Action {
-    private final VisualizationViewer<PetriNetVertex, Arc> graphViewer;
-    private final SimulationPanel simulationPanel;
-
-    public StartSimulationModeAction(VisualizationViewer<PetriNetVertex, Arc> graphViewer,
-                                     SimulationPanel simulationPanel) {
-        this.graphViewer = graphViewer;
-        this.simulationPanel = simulationPanel;
-    }
-
     @Override
     public void invoke() {
-        PetriLabApplication.getInstance().loadSimulationGraph();
-        PetriLabApplication.getInstance().getConfiguration().setProperty(SIMULATION_MODE_ACTIVE, true);
-        VisualizationServer.Paintable activeTransitionPreRenderPaintable = new ActiveTransitionsBoundingRectanglePaintable(
-                graphViewer.getRenderContext(), graphViewer.getGraphLayout());
-        PetriLabApplication.getInstance().getConfiguration()
-                .setProperty(SIMULATION_ACTIVE_TRANSITIONS_PRE_RENDER_PAINTABLE, activeTransitionPreRenderPaintable);
-        graphViewer.addPreRenderPaintable(activeTransitionPreRenderPaintable);
-        graphViewer.repaint();
-        simulationPanel.initMarking();
+        PetriLabApplication.getInstance().getModeManager().setSimulationMode();
     }
 }
--- petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/action/StopSimulationModeAction.java
+++ petrilab-gui/src/main/java/pl/edu/agh/eis/petrilab/gui/menu/action/StopSimulationModeAction.java
@@ -1,13 +1,6 @@
 package pl.edu.agh.eis.petrilab.gui.menu.action;
 
-import edu.uci.ics.jung.visualization.VisualizationServer;
-import edu.uci.ics.jung.visualization.VisualizationViewer;
 import pl.edu.agh.eis.petrilab.gui.PetriLabApplication;
-import pl.edu.agh.eis.petrilab.model2.Arc;
-import pl.edu.agh.eis.petrilab.model2.PetriNetVertex;
-
-import static pl.edu.agh.eis.petrilab.gui.Configuration.SIMULATION_ACTIVE_TRANSITIONS_PRE_RENDER_PAINTABLE;
-import static pl.edu.agh.eis.petrilab.gui.Configuration.SIMULATION_MODE_ACTIVE;
 
 /**
  * Name: StopSimulationMode
@@ -16,19 +9,8 @@ import static pl.edu.agh.eis.petrilab.gui.Configuration.SIMULATION_MODE_ACTIVE;
  * Created by BamBalooon
  */
 public class StopSimulationModeAction implements Action {
-    private final VisualizationViewer<PetriNetVertex, Arc> graphViewer;
-
-    public StopSimulationModeAction(VisualizationViewer<PetriNetVertex, Arc> graphViewer) {
-        this.graphViewer = graphViewer;
-    }
-
     @Override
     public void invoke() {
-        PetriLabApplication.getInstance().loadPetriNetGraph();
-        PetriLabApplication.getInstance().getConfiguration().setProperty(SIMULATION_MODE_ACTIVE, false);
-        VisualizationServer.Paintable activeTransitionPreRenderPaintable = PetriLabApplication.getInstance()
-                .getConfiguration().removeProperty(SIMULATION_ACTIVE_TRANSITIONS_PRE_RENDER_PAINTABLE);
-        graphViewer.removePreRenderPaintable(activeTransitionPreRenderPaintable);
-        graphViewer.repaint();
+        PetriLabApplication.getInstance().getModeManager().setNormalMode();
     }
 }
